9e6b766eeb3045ee52a1b260fe48e398ba36219d
Joe Potentier <joseph.potentier@gmail.com>
2020-01-14 00:02:53 -0500

    printMyself removed token

--- lab1/printMyself.py
+++ lab1/printMyself.py
@@ -1,3 +1,3 @@
 import requests
-x = requests.get('https://raw.githubusercontent.com/JoePotentier/cmput-404-labs/master/lab1/printMyself.py?token=ACJZXVDB33YNYQ5SNB6F66C6E2B3I')
+x = requests.get('https://raw.githubusercontent.com/JoePotentier/cmput-404-labs/master/lab1/printMyself.py')
 print(x.text)
